27b438cacfd75bbea06b3392e471f703e4f08107
Kolawura <adekolaadediwura@gmail.com>
2023-10-02 14:02:13 +0100

    Deleted the quote.json file and clean up the fetch.js file

--- fetch.js
+++ fetch.js
@@ -1,47 +1,26 @@
 const quote = document.querySelector(".quotes");
 const author = document.querySelector(".author");
-const nextBtn = document.querySelector('.newQ');
+const nextBtn = document.querySelector(".newQ");
 
-nextBtn.addEventListener('click', getQuote);
+nextBtn.addEventListener("click", getQuote);
 
-addEventListener('keydown', function (e) {
-    if(e.key == 'Enter'){
-        getQuote(e);
-    }
-})
+addEventListener("keydown",(e) => {
+  if (e.key == "Enter") {
+    getQuote(e);
+  }
+});
 
-function getQuote (e){
-    e.preventDefault();
-    
-    fetch("quote.json")
-        .then(function(response){
-        return response.json();
-        })
-        .then(function(data){
-            
+function getQuote(e) {
+  e.preventDefault();
 
-            randomValue = Math.floor(Math.random() * data.length)
+  fetch("quote.json")
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      randomValue = Math.floor(Math.random() * data.length);
 
-                quote.innerText = data[randomValue].text;
-                author.innerText = data[randomValue].Author.toUpperCase();
-        })
-
-    // let i = 0;
-    // while (i<13) {
-    //     console.log(i);
-    //     i++
-    // }           
-        
-}; 
-
-// function shuffle(quote) {
-//     let CI = quote.length, tempValue, randomValue;
-
-//     while (CI > 0) {
-//         randomValue = Math.floor(Math.random() * quote.length);
-//         CI--;
-//         tempValue = quote[CI];
-//         quote[CI] = quote[randomValue];
-//         quote[randomValue] = tempValue;
-//     }
-// }
+      quote.innerText = data[randomValue].text;
+      author.innerText = data[randomValue].Author.toUpperCase();
+    });
+}
