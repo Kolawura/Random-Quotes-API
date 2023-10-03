98dbd996c1b6667d140dfcb3498eabcd4bd6fc7f
Kolawura <adekolaadediwura@gmail.com>
2023-10-03 12:01:05 +0100

    clean up the fetch.js code to display the right author

--- fetch.js
+++ fetch.js
@@ -4,23 +4,31 @@ const nextBtn = document.querySelector(".newQ");
 
 nextBtn.addEventListener("click", getQuote);
 
-addEventListener("keydown",(e) => {
+addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     getQuote(e);
   }
 });
 
+
 function getQuote(e) {
   e.preventDefault();
 
   fetch("https://type.fit/api/quotes")
-    .then((response) => {
-      response.json();
-    })
+    .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       randomValue = Math.floor(Math.random() * data.length);
-
+     
       quote.innerText = data[randomValue].text;
-      author.innerText = data[randomValue].Author.toUpperCase();
-    });
+      let authors = data[randomValue].author;
+      let authorName = authors.split(",")
+      if(authorName.length ===1 && authorName[0] === 'type.fit'){
+        author.innerText = 'ANONYMOUS';
+      }else {
+        author.innerText = authorName[0].toLocaleUpperCase();
+      }
+      
+    })
+    .catch((error) => console.error("Error fetching data: ", error));
 }
